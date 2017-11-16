e810111106fd804d882c5aff195e4bf2ccf677f7
chenghua <chehua263859@163.com>
2017-11-16 15:04:21 +0800

    路由文件加载方式更改，前端无须关注路由加载，只需添加自己所需路由即可

--- routers/index.js
+++ routers/index.js
@@ -4,13 +4,31 @@
 const fs = require('fs')
 const routerPath = __dirname
 
+const getRouters = (routerList, path) => {
+    const files = fs.readdirSync(path)
+    files.forEach((each) => {
+        const state = fs.statSync(`${path}/${each}`)
+        if (state.isFile()) {
+            routerList.push(`${path}/${each}`)
+        }
+        if (state.isDirectory()) {
+            getRouters(routerList, `${path}/${each}`)
+        }
+    })
+    return routerList
+}
+
 module.exports = (app) => {
 
-    const routerList = fs.readdirSync(routerPath)
+    const routerList = getRouters([], routerPath)
+    console.log('################ <Router load start .....> ##################')
     routerList.forEach( (file) => {
-        if (file !== 'index.js') {
-            const eachRouter = require(`./${file}`)
+        file = `.${file.split(routerPath)[1]}`
+        if (file !== './index.js') {
+            console.log(`router: ${file} ......`)
+            const eachRouter = require(`${file}`)
             app.use(eachRouter.routes(), eachRouter.allowedMethods())
         }
     })
+    console.log('################ <Router load success .....> ################')
 }
