e8e0620a01111f69f91978a0e647b72e1cc6533c
chenghua <chehua263859@163.com>
2017-11-16 14:05:22 +0800

    修改路由加载方式

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,16 @@
 
 'use strict'
 
-const xcRouter = require('./xc')
+const fs = require('fs')
+const routerPath = __dirname
 
 module.exports = (app) => {
-    app.use(xcRouter.routes(), xcRouter.allowedMethods())
+
+    const routerList = fs.readdirSync(routerPath)
+    routerList.forEach( (file) => {
+        if (file !== 'index.js') {
+            const eachRouter = require(`./${file}`)
+            app.use(eachRouter.routes(), eachRouter.allowedMethods())
+        }
+    })
 }
