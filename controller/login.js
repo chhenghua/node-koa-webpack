ceac8440c8380bdd1190f1aeb585eb7923b10106
chenghua <chehua263859@163.com>
2017-11-22 17:01:37 +0800

    add token verify

--- controller/login.js
+++ controller/login.js
@@ -5,12 +5,18 @@ const config = require('../config')
 
 //密码登录
 exports.apiManagerStorePWDLogin = async (ctx) => {
-    const query = ctx.request.query
+    // const query = ctx.request.query
     const body = ctx.request.body
-    return await request.post({
+
+    const loginRlt = await request.post({
         url: `${config.host}/api/admin/ManagerStorePWDLogin`,
         form: body
     })
+
+    // 将从后端拿到的token写到session
+    ctx.session.web_token = loginRlt.token
+
+    return loginRlt
 }
 
 //短信登录
